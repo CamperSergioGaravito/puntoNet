e5fd4a68e652e7c32c7daf1f5ebc738ade6a5ba6
CamperSergioGaravito <137243895+CamperSergioGaravito@users.noreply.github.com>
2023-09-10 04:49:42 -0500

    v0.0.2
    Agregado aviso de página en construcción

--- js/nav.js
+++ js/nav.js
@@ -1,39 +1,48 @@
+console.log(window.location)
+let ruta
+switch(window.location.pathname) {
+    case "/index.html":
+        ruta = './assets'
+    case "/pages/construccion.html":
+        ruta = '../assets'
+}
+
 export const navTemplate = //html
                 `
                 <ul class="nav__ul">
                     <li class="nav__li">
                         <a href="../index.html" class="nav__a">
-                            <img src="./assets/iconos/home.svg" alt="" class="nav__imgIco">
+                            <img src="${ruta}/iconos/home.svg" alt="" class="nav__imgIco">
                             Home
                         </a>
                     </li>
                     <li class="nav__li">
-                        <a href="#" class="nav__a">
-                            <img src="./assets/iconos/list-box.svg" alt="" class="nav__imgIco">
+                        <a href="../pages/construccion.html" class="nav__a">
+                            <img src="${ruta}/iconos/list-box.svg" alt="" class="nav__imgIco">
                             Historia
                         </a>
                     </li>
                     <li class="nav__li">
-                        <a href="#" class="nav__a">
-                            <img src="./assets/iconos/biografia.svg" alt="" class="nav__imgIco">
+                        <a href="../pages/construccion.html" class="nav__a">
+                            <img src="${ruta}/iconos/biografia.svg" alt="" class="nav__imgIco">
                             Biografías
                         </a>
                     </li>
                     <li class="nav__li">
-                        <a href="#" class="nav__a">
-                            <img src="./assets/iconos/cd.svg" alt="" class="nav__imgIco">
+                        <a href="../pages/construccion.html" class="nav__a">
+                            <img src="${ruta}/iconos/cd.svg" alt="" class="nav__imgIco">
                             Sistemas
                         </a>
                     </li>
                     <li class="nav__li">
-                        <a href="#" class="nav__a">
-                            <img src="./assets/iconos/folder-tree.svg" alt="" class="nav__imgIco">
+                        <a href="../pages/construccion.html" class="nav__a">
+                            <img src="${ruta}/iconos/folder-tree.svg" alt="" class="nav__imgIco">
                             Tabla evolutiva
                         </a>
                     </li>
                     <li class="nav__li">
-                        <a href="#" class="nav__a">
-                            <img src="./assets/iconos/bibliografia.svg" alt="" class="nav__imgIco">
+                        <a href="../pages/construccion.html" class="nav__a">
+                            <img src="${ruta}/iconos/bibliografia.svg" alt="" class="nav__imgIco">
                             Bibliografía
                         </a>
                     </li>
