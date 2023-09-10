0bc691a9aa6110829f8a54fd161a066d6e795a82
CamperSergioGaravito <137243895+CamperSergioGaravito@users.noreply.github.com>
2023-09-10 05:17:49 -0500

    Update js

--- js/nav.js
+++ js/nav.js
@@ -1,42 +1,48 @@
 console.log(window.location)
-let ruta = "/puntoNet/"
+let ruta
+if(window.location.pathname === "/index.html") {
+    ruta = './'
+}
+else {
+    ruta = '../'
+}
 
 export const navTemplate = //html
                 `
                 <ul class="nav__ul">
                     <li class="nav__li">
                         <a href="${ruta}index.html" class="nav__a">
-                            <img src="${ruta}assets/iconos/home.svg" alt="" class="nav__imgIco">
+                            <img src="${ruta}/assets/iconos/home.svg" alt="" class="nav__imgIco">
                             Home
                         </a>
                     </li>
                     <li class="nav__li">
-                        <a href="${ruta}/pages/construccion.html" class="nav__a">
+                        <a href="${ruta}pages/construccion.html" class="nav__a">
                             <img src="${ruta}/assets/iconos/list-box.svg" alt="" class="nav__imgIco">
                             Historia
                         </a>
                     </li>
                     <li class="nav__li">
                         <a href="${ruta}pages/construccion.html" class="nav__a">
-                            <img src="${ruta}assets/iconos/biografia.svg" alt="" class="nav__imgIco">
+                            <img src="${ruta}/assets/iconos/biografia.svg" alt="" class="nav__imgIco">
                             Biografías
                         </a>
                     </li>
                     <li class="nav__li">
                         <a href="${ruta}pages/construccion.html" class="nav__a">
-                            <img src="${ruta}assets/iconos/cd.svg" alt="" class="nav__imgIco">
+                            <img src="${ruta}/assets/iconos/cd.svg" alt="" class="nav__imgIco">
                             Sistemas
                         </a>
                     </li>
                     <li class="nav__li">
                         <a href="${ruta}pages/construccion.html" class="nav__a">
-                            <img src="${ruta}assets/iconos/folder-tree.svg" alt="" class="nav__imgIco">
+                            <img src="${ruta}/assets/iconos/folder-tree.svg" alt="" class="nav__imgIco">
                             Tabla evolutiva
                         </a>
                     </li>
                     <li class="nav__li">
                         <a href="${ruta}pages/construccion.html" class="nav__a">
-                            <img src="${ruta}assets/iconos/bibliografia.svg" alt="" class="nav__imgIco">
+                            <img src="${ruta}/assets/iconos/bibliografia.svg" alt="" class="nav__imgIco">
                             Bibliografía
                         </a>
                     </li>
